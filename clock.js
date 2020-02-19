c68630bc584605c298754eac0225ab61e807a887
Chris Norman <chris.norman2@googlemail.com>
2020-02-19 21:28:08 +0000

    First commit.

---
+++ clock.js
@@ -0,0 +1,52 @@
+const long = 300
+const short = 100
+
+const hours = document.querySelector("#hours")
+const minutes = document.querySelector("#minutes")
+const seconds = document.querySelector("#seconds")
+const vibrate= document.querySelector("#vibrate")
+let now = new Date()
+
+window.onload = () => {
+    setInterval(() => {
+        now = new Date()
+        hours.value = now.getHours().toString()
+        minutes.value = now.getMinutes().toString()
+        seconds.value = now.getSeconds().toString()
+    }, 1000)
+}
+
+function divmod(x, y) {
+    return [Math.floor(x / y), x % y]
+}
+
+function numberToBeeps(number) {
+    let res = []
+    let [tens, units] = divmod(number, 10)
+    for (let i = 0; i < tens; i++) {
+        res.push(long)
+    }
+    for (let i = 0; i < units; i++) {
+        res.push(short)
+    }
+    return res
+}
+
+hours.onclick = () => {
+    navigator.vibrate(numberToBeeps(now.getHours()))
+}
+
+minutes.onclick = () => {
+    navigator.vibrate(numberToBeeps(now.getMinutes()))
+}
+
+seconds.onclick = () => {
+    navigator.vibrate(numberToBeeps(now.getSeconds()))
+}
+
+vibrate.onclick = () => {
+    let hourBeeps = numberToBeeps(now.getHours())
+    let minuteBeeps = numberToBeeps(now.getMinutes())
+    let beeps = hourBeeps.concat(minuteBeeps)
+    navigator.vibrate(beeps)
+}
